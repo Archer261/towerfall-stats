f2e8ef56ccf0ebaa31c6def1539a118e15789caa
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-01 20:42:16 -0400

    second commit

--- database/db.sql
+++ database/db.sql
@@ -1,19 +1,19 @@
 -- to create a new database
-CREATE DATABASE nodejs2;
+CREATE DATABASE towerfallstats;
 
 -- to use database
-use nodejs2;
+use towerfallstats;
 
 -- creating a new table
-CREATE TABLE customer (
+CREATE TABLE player (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  phone VARCHAR(15)
+  tag VARCHAR(100) NOT NULL,
+  color VARCHAR(15)
 );
 
 -- to show all tables
 show tables;
 
 -- to describe table
-describe customer;
+describe player;
